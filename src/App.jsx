88245edf53d874124b51edcf4f6207c29cfc8fa9
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-07-24 15:29:47 -0400

    Completed logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Cards from "./flashcards.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [cardIndex, setCardIndex] = useState(0);
+  const [cards, setCards] = useState(() =>
+    Cards.map(card => ({
+      ...card,
+      showingQuestion: true,
+    }))
+  );
+
+  const { question, answer, showingQuestion } = cards[cardIndex];
+
+  function toggle() {
+    let newCards = [...cards];
+    newCards[cardIndex] = {
+      ...newCards[cardIndex],
+      showingQuestion: !newCards[cardIndex].showingQuestion,
+    };
+    setCards(newCards);
+  }
+
+  function prevCard() {
+    setCardIndex((prev) => prev - 1);
+  }
+
+  function nextCard() {
+    setCardIndex((prev) => prev + 1);
+  }
 
   return (
-    <>
+    <div>
+      {showingQuestion ? <h4>{question}</h4> : <p>{answer}</p>}
+      <button onClick={toggle}>
+        {showingQuestion ? "Show" : "Hide"} Answer
+      </button>
+      <br />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="previous-btn" onClick={prevCard} disabled={cardIndex === 0}>
+          &lt;
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="next-btn" onClick={nextCard} disabled={cardIndex === cards.length - 1}>
+          &gt;
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
---
+++ src/flashcards.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        question: "Who is the tallest person in the world?",
+        answer: "Sultan Kosen",
+        showingQuestion: false
+    },
+    {
+        question: "Who discovered gravity?",
+        answer: "Isaac Newton",
+        showingQuestion: false
+    },
+    {
+        question: "Who invented the telephone",
+        answer: "Alexander Graham Bell",
+        showingQuestion: false
+    },
+]
